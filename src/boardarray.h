f71aa23d92ed4ca434251b9c6eb005b74122f587
unknown <Raul Abanto>
2025-09-20 17:16:16 +0800

    feat: implement add() method for boardarray

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,33 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            int pos = index;
+
+            for(int i = 0;i < index;i++){
+                if(entry->compare(&array[i])){
+                    pos = i; break;
+                }
+            }
+
+            if(index < SIZE){
+                for(int i = index;i > pos;i--){
+                    array[i] = array[i - 1];
+                }
+                array[pos] = *entry;
+                index++;
+            }
+            else{
+                if(pos < SIZE){
+                    for(int j = index - 1;j > pos;j--){
+                        array[j] = array[j - 1];
+                    }
+
+                    array[pos] = *entry;
+                }
+                else{
+                    cout << entry->name << "'s score is too low to be added!\n";
+                }
+            }
 
         }
 
